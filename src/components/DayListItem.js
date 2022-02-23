21cb0dd20738b0c546e11ef32ccbb85bb58c29f5
Jiani Cao <jianicakes@gmail.com>
2022-02-22 19:33:13 -0800

    add classes to <li>

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -6,8 +6,14 @@ import "components/DayListItem.scss";
 export default function DayListItem(props) {
   const { name, spots, selected, setDay } = props;
 
+  const dayClass = classNames("day-list__item", {
+    "day-list__item--selected": selected,
+    "day-list__item--full": !spots
+  });
+
   return (
     <li
+      className={dayClass}
       onClick={() => setDay(name)}
     >
       <h2 className="text--regular">{name}</h2> 
