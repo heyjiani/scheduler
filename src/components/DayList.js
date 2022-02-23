7efae8a6b7abe83c6aa2b805790a03d6379f7247
Jiani Cao <jianicakes@gmail.com>
2022-02-22 20:35:30 -0800

    render each item component in DayList

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -3,7 +3,19 @@ import React from "react";
 import DayListItem from "./DayListItem";
 
 export default function DayList(props) {
+  const { days, day, setDay } = props;
+
+  const parsedDays =
+    Array.isArray(days) && days.map(dayItem => {
+    dayItem.selected = (day === dayItem.name) ? true : false;
+    dayItem.setDay = setDay;
+
+    return <DayListItem key={dayItem.id} {...dayItem}/>
+  });
+
   return (
-    <ul></ul>
+    <ul>
+      { parsedDays }
+    </ul>
   )
 }
--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -23,6 +23,7 @@ export default function DayListItem(props) {
     <li
       className={dayClass}
       onClick={() => setDay(name)}
+      selected={selected}
     >
       <h2 className="text--regular">{name}</h2> 
       <h3 className="text--light">{formatSpots(spots)}</h3>
