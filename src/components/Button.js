7bcfdcae9d64866c8395f1c0ac4a13749016308d
Jiani Cao <jianicakes@gmail.com>
2022-02-22 19:17:06 -0800

    remove extra space

--- src/components/Button.js
+++ src/components/Button.js
@@ -13,7 +13,7 @@ export default function Button(props) {
       <button
          className={buttonClass}
          onClick={props.onClick}
-         disabled = {props.disabled}
+         disabled={props.disabled}
       >
          {props.children}
       </button>
