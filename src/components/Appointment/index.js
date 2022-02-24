d9eb11a8f4afe1a0cd2693f28004fef9189f6587
Jiani Cao <jianicakes@gmail.com>
2022-02-23 22:38:51 -0800

    set up basic tests for Appointment component

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,7 +3,11 @@ import React from "react";
 import "components/Appointment/styles.scss";
 
 export default function Appointment(props) {
+  const { time } = props;
+
   return (
-    <article className="appointment"></article>
+    <article className="appointment">
+      {time ? <span>Appointment at {time}</span> : <span>No Appointments</span>}
+    </article>
   );
 }
