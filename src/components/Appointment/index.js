8538bea4df4dda7c969a7fb4a7dfba5052de1613
Jiani Cao <jianicakes@gmail.com>
2022-02-24 00:01:10 -0800

    import appointment components and add tests

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,13 +1,18 @@
 import React from "react";
 
+import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
+
 import "components/Appointment/styles.scss";
 
 export default function Appointment(props) {
-  const { time } = props;
+  const { time, interview } = props;
 
   return (
     <article className="appointment">
-      {time ? <span>Appointment at {time}</span> : <span>No Appointments</span>}
+      <Header time={time} />
+      {props.interview ? <Show student={interview.student} interviewer={interview.interviewer}/> : <Empty />}
     </article>
   );
 }
