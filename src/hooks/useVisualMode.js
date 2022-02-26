c66992049d780d4bb168377eb298ec4a136c42ff
Jiani Cao <jianicakes@gmail.com>
2022-02-25 19:57:11 -0800

    implement transition functionality and test

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,7 +1,11 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
 
-  return { mode }
+  const transition = (newMode) => {
+    setMode(newMode);
+  };
+
+  return { mode, transition };
 };
