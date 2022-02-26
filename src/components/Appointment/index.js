3d46776ed7d66c0f4ec0b6064e36861f0f2f7a80
Jiani Cao <jianicakes@gmail.com>
2022-02-26 01:56:36 -0800

    pass parsed interviewer data to form component

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,16 +3,39 @@ import React from "react";
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
+import Form from "./Form";
+
+import useVisualMode from "hooks/useVisualMode";
 
 import "components/Appointment/styles.scss";
 
 export default function Appointment(props) {
-  const { time, interview } = props;
+  const { time, interview, interviewers } = props;
+
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+  const CREATE = "CREATE";
+
+  const { mode, transition, back } = useVisualMode(
+      interview ? SHOW : EMPTY
+    );
 
   return (
     <article className="appointment">
       <Header time={time} />
-      {props.interview ? <Show student={interview.student} interviewer={interview.interviewer}/> : <Empty />}
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === SHOW && (
+        <Show
+          student={interview.student}
+          interviewer={interview.interviewer}
+        />
+      )}
+      {mode === CREATE && (
+        <Form
+          interviewers={interviewers}
+          onCancel={() => back()}
+        />
+      )}
     </article>
   );
 }
