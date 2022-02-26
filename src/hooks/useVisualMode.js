69206b453f115603c6e7d15667bf47227593ca4d
Jiani Cao <jianicakes@gmail.com>
2022-02-25 20:50:12 -0800

    implement back & replace functionality and tests

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -2,10 +2,27 @@ import React, { useState } from "react";
 
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
 
-  const transition = (newMode) => {
+  const transition = (newMode, replace = false) => {
+    if (replace) {
+      return setMode(newMode);
+    }
+
     setMode(newMode);
+    setHistory(prev => [...prev, newMode]);
   };
 
-  return { mode, transition };
+  const back = () => {
+    if (history.length < 2) {
+      return;
+    }
+
+    const prevMode = history[history.length - 2];
+    setMode(prevMode);
+
+    setHistory(history.slice(0, -1));
+  };
+
+  return { mode, transition, back };
 };
