b9e93db4e92fc7b07c2322f3015ec41e1e1ee1a7
Jiani Cao <jianicakes@gmail.com>
2022-03-01 15:31:21 -0800

    implement feature for updating remaining spots

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -23,6 +23,21 @@ export default function useApplicationData() {
   //set selected day as current day
   const setDay = day => setState({ ...state, day });
 
+  /* update number of available spots for current day
+  * finds current day, updates spot number using new appointment data, and returns a Days array
+  */
+  const updateSpots = function(state, appointments, id) {
+
+    const currentDay = state.days.find(day => day.appointments.includes(id));
+    let spots = 0;
+  
+    currentDay.appointments.forEach(id => {
+      spots = !appointments[id].interview ? spots + 1 : spots;
+    })
+  
+    return state.days.map(day => ({ ...day, spots }));
+  };
+
   /* add or update an interview to local & database
   * sends put request with interview data in the form of an obj & returns a promise
   */
@@ -37,9 +52,13 @@ export default function useApplicationData() {
       [id]: appointment
     };
 
+    const days = updateSpots(state, appointments, id);
+
     return axios
       .put(`/api/appointments/${id}`, { interview })
-      .then(res => setState({ ...state, appointments}))
+      .then(res => {
+        setState({ ...state, appointments, days })
+      })
   };
 
   /* delete an interview from local & database
@@ -56,9 +75,11 @@ export default function useApplicationData() {
       [id]: appointment
     };
 
+    const days = updateSpots(state, appointments, id);
+
     return axios
       .delete(`/api/appointments/${id}`)
-      .then(res => setState({ ...state, appointments}))
+      .then(res => setState({ ...state, appointments, days}))
   };
 
   return {
