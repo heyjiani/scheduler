a63957eba22474a9b2207b846075f52b60a205a4
Jiani Cao <jianicakes@gmail.com>
2022-02-28 20:56:10 -0800

    add loading status messages

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -4,12 +4,12 @@ import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
 import Form from "./Form";
+import Status from "./Status";
+import Confirm from "./Confirm";
 
 import useVisualMode from "hooks/useVisualMode";
 
 import "components/Appointment/styles.scss";
-import Status from "./Status";
-import Confirm from "./Confirm";
 
 export default function Appointment(props) {
   const { id, time, interview, interviewers, bookInterview, cancelInterview } = props;
@@ -18,6 +18,7 @@ export default function Appointment(props) {
   const SHOW = "SHOW";
   const CREATE = "CREATE";
   const SAVING = "SAVING";
+  const DELETING = "DELETING";
   const CONFIRM = "CONFIRM";
 
   const { mode, transition, back } = useVisualMode(
@@ -32,13 +33,16 @@ export default function Appointment(props) {
 
     transition(SAVING);
 
-    bookInterview(id , interview)
+    setTimeout(() => {
+      bookInterview(id , interview)
       .then(transition(SHOW))
-      .catch(err => console.log(err.message));
+      .catch(err => console.log(err.message))
+    }, 1200)
+
   };
 
   const handleDelete = () => {
-    transition(SAVING);
+    transition(DELETING);
 
     setTimeout(() => {
       cancelInterview(id)
@@ -58,7 +62,8 @@ export default function Appointment(props) {
           onDelete={() => transition(CONFIRM)}
         />
       )}
-      {mode === SAVING && <Status message={'Loading...'}/>}
+      {mode === SAVING && <Status message={'Saving...'}/>}
+      {mode === DELETING && <Status message={'Deleting...'}/>}
       {mode === CONFIRM && (
         <Confirm
           message={'Are you sure you would like to delete this interview?'}
