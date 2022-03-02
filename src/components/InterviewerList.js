bd8dcd1daad691824d91555c77d93a5c87587336
Jiani Cao <jianicakes@gmail.com>
2022-03-01 17:26:43 -0800

    enable runtime type checking for interviewers props  with PropTypes

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,10 +1,11 @@
 import React from "react";
+import PropTypes from 'prop-types';
 
 import InterviewerListItem from "./InterviewerListItem";
 
 import "components/InterviewerList.scss";
 
-export default function InterviewerList(props) {
+function InterviewerList(props) {
   const { interviewers, value, onChange } = props;
 
   const parsedInterviewers =
@@ -28,3 +29,9 @@ export default function InterviewerList(props) {
     </section>
   )
 };
+
+InterviewerList.propTypes = {
+  interviewers: PropTypes.array.isRequired
+};
+
+export default InterviewerList;
