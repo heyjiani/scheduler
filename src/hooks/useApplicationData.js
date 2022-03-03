8814c4c79fdd4f1ae48405be3053d39947f70acb
Jiani Cao <jianicakes@gmail.com>
2022-03-02 16:21:17 -0800

    set up useReducer work in progress

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -3,6 +3,27 @@ import axios from "axios";
 
 export default function useApplicationData() {
 
+  /** ------- work in progress -------- */
+  // const SET_DAY = "SET_DAY";
+  // const SET_APPLICATION_DATA = "SET_APPLICATION_DATA";
+  // const SET_INTERVIEW = "SET_INTERVIEW";
+
+  // const reducer = (state, action) => {
+  //   switch (action.type) {
+  //     case SET_DAY:
+  //       return { /* insert logic */ }
+  //     case SET_APPLICATION_DATA:
+  //       return { /* insert logic */ }
+  //     case SET_INTERVIEW: {
+  //       return /* insert logic */
+  //     }
+  //     default:
+  //       throw new Error(
+  //         `Tried to reduce with unsupported action type: ${action.type}`
+  //       );
+  //   }
+  // }
+
   const [state, setState] = useState({
     day: "Monday",
     days: [],
